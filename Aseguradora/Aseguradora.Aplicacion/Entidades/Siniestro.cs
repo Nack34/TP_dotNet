1003c9ef98ef118cd898ae1c1baeb748fb503a47
Srteddy14 <ridolfidaluz.a@gmail.com>
2023-05-08 21:19:06 -0300

    Borre email y direccion de siniestro

--- Aseguradora/Aseguradora.Aplicacion/Entidades/Siniestro.cs
+++ Aseguradora/Aseguradora.Aplicacion/Entidades/Siniestro.cs
@@ -3,8 +3,6 @@ namespace Aseguradora.Aplicacion;
 public class Siniestro { 
     public int ID {get; set;} = -1; 
     public int PolizaId {get;set;}
-    public string Direccion {get; set;} = "No tiene direccion";
-    public string Email {get; set;} = "No tiene email";
     public string DireccionDelHecho {get; set;} = "No tiene direccion";
     public string DescripciónDelAccidente {get; set;} = "No tiene descripcion";
     public DateTime FechaDeingreso {get;set;}
@@ -14,8 +12,6 @@ public class Siniestro {
     public Siniestro(int polizaId, String direccion, String email,String direccionDelHecho, String descripciónDelAccidente, 
                     DateTime fechaDeingreso, DateTime fechaDeOcurrencia){
         PolizaId=polizaId;
-        Direccion=direccion;
-        Email=email;
         DireccionDelHecho=direccionDelHecho;
         DescripciónDelAccidente = descripciónDelAccidente;
         FechaDeingreso=DateTime.Now;
@@ -23,8 +19,7 @@ public class Siniestro {
     }
 
     public override String ToString(){
-        return $"ID: {ID}, PolizaId: {PolizaId}, Direccion: {Direccion}, "+ 
-        $"Email: {Email}, DireccionDelHecho: {DireccionDelHecho}, DescripciónDelAccidente: {DescripciónDelAccidente}, "+
+        return $"ID: {ID}, PolizaId: {PolizaId}, DireccionDelHecho: {DireccionDelHecho}, DescripciónDelAccidente: {DescripciónDelAccidente}, "+
         $"FechaDeingreso: {FechaDeingreso}, FechaDeOcurrencia: {FechaDeOcurrencia}";
     }
 
